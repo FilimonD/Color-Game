737cdd8b6d718b60be9c24e13af2f2f4a8f02c15
abnk068 <filimon.daniel68@gmail.com>
2020-09-10 15:17:07 +0100

    added  button event listeners

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,7 @@
 // generate random colors with 6 options(colored squares)
-let colors = generateRandomColors(6);
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
+
 let h1 = document.querySelector("h1");
 
 let squares = document.querySelectorAll(".square");
@@ -9,6 +11,55 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 let messageDisplay = document.querySelector("#message");
+let resetButton = document.querySelector("#reset");
+let easyBtn = document.querySelector("#easyBtn");
+let hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function() {
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+});
+
+resetButton.addEventListener("click", function() {
+  // generate all new colors
+  colors = generateRandomColors(numSquares);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  // change color display to match picked color
+  colorDisplay.textContent = pickedColor;
+  // change color of squares
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+});
 
 for (let i = 0; i < squares.length; i++) {
   // add initial colors to squares
@@ -23,6 +74,7 @@ for (let i = 0; i < squares.length; i++) {
 
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct";
+      resetButton.textContent = "Play Again!";
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
     } else {
